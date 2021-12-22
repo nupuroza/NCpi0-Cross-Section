00100bafbd3e62179383290ffaaa97776689bf7b
Rob Fine <finer@uboonegpvm04.fnal.gov>
2021-12-22 15:14:13 -0600

    remove code to produce 'pretty' plots. That was broken by the inclusive/exclusive syntax, and will have to be reworked significantly anyways

--- makePlots.py
+++ makePlots.py
@@ -101,7 +101,6 @@ for sigDef in ["2g0p","2g1p","2gnp"]:
     if sigDef == "2gnp" and sigDefexcl == "exclusive":
       continue
 
-<<<<<<< Updated upstream
     else:
       for histCat in ["effDenom", "eff", "xSection", "xSection_mc"]:
         exec("{0}_{1}_{2} = histFile.Get(\"{0}_{1}_{2}\")".format(histCat,sigDef,sigDefexcl))
@@ -139,12 +138,9 @@ for sigDef in ["2g0p","2g1p","2gnp"]:
         err_val_stat = local_hist_statError.GetBinError(1)
         err_val_syst = local_hist_systError.GetBinError(1)
         print "sigDef: {0}\thistCat: {1}_{2}\tcv_val: {3}\terr_val_stat: {4}\terr_val_syst: {5}\terr_val_tot: {6}".format(histCat,sigDef,sigDefexcl,cv_val,err_val_stat,err_val_syst,err_val)
-
 		
   for histCat in ["effNum","data_selected","BNB_ext","background","evtRate","POT"]:
-=======
     print "I'm producing plots for histCat: {0}".format(histCat)
->>>>>>> Stashed changes
     exec("{0}_{1} = histFile.Get(\"{0}_{1}\")".format(histCat,sigDef))
 
     with makeEnv_TCanvas("{0}/{1}_{2}.png".format(plotDir,histCat,sigDef)):
@@ -219,124 +215,4 @@ for syst_uni in ["piplus_PrimaryHadronSWCentralSplineVariation","All_UBGenie"]:
     exec("local_xSection_2g1p_{1}_breakout = xSection_2g1p_{1}.GetVertErrorBand(\"{0}\")".format(syst_uni,sigDefexcl))
     with makeEnv_TCanvas("{0}/breakout_{1}_xSection.png".format(plotDir,syst_uni)):
       exec("local_xSection_2g1p_{0}_breakout.DrawAll(\"\",True)".format(sigDefexcl))
-
-#############################################################################################################
-### Make pretty cross section plot ##########################################################################
-#############################################################################################################
-with makeEnv_TCanvas("{0}/xSection_pretty.png".format(plotDir)):
-
-  local_xSection_2g0p.Scale(10**38)
-  local_xSection_2g0p.GetYaxis().SetTitle("#sigma_{NC 1 #pi^{0}} [10^{-38} cm^{2}/Atom]")
-  local_xSection_2g0p.GetYaxis().SetRangeUser(0,2)
-  local_xSection_2g0p.Draw()
-  
-  local_xSection_2g1p.Scale(10**38)
-  local_xSection_2g1p.SetMarkerColor(ROOT.kRed)
-  local_xSection_2g1p.SetLineColor(ROOT.kRed)
-  local_xSection_2g1p.Draw("same")
-
-  local_xSection_2gnp.Scale(10**38)
-  local_xSection_2gnp.SetMarkerColor(ROOT.kBlue)
-  local_xSection_2gnp.SetLineColor(ROOT.kBlue)
-  local_xSection_2gnp.Draw("same")
-
-  ############################################################### Legend
-  ######################################################################
-
-  leg = ROOT.TLegend(0.55,0.7,0.95,0.9)
-  setPlotSpecs_legend(leg)
-  leg.AddEntry(local_xSection_2g0p,"2g0p","lep")
-  leg.AddEntry(local_xSection_2g1p,"2g1p","lep")
-  leg.AddEntry(local_xSection_2gnp,"2g(0+1)p","lep")
-  leg.Draw()
-
-#############################################################################################################
-### Mark's pretty cross section plot code ###################################################################
-#############################################################################################################
-
-with makeEnv_TCanvas("{0}/xSection_pretty_markFormat.png".format(plotDir)):
-
-  ROOT.gStyle.SetOptStat(0)
-  ROOT.gStyle.SetEndErrorSize(7)
-  ROOT.gStyle.SetErrorX(0.00001)
-
-  local_xSection_2g0p.SetLineColor(ROOT.kGreen-3)
-  local_xSection_2g0p.SetMarkerColor(ROOT.kGreen-3)
-  local_xSection_2g0p.SetLineWidth(2)
-  local_xSection_2g0p.SetMarkerStyle(20)
-
-  local_xSection_2g0p.Draw("E1p")
-  
-  local_xSection_2g0p.SetMaximum(3.0)
-  local_xSection_2g0p.SetMinimum(1.0)
-  local_xSection_2g0p.GetYaxis().SetTitle("#sigma_{NC 1 #pi^{0} +0 p} [10^{-38} cm^{2}/Atom]")
-  local_xSection_2g0p.GetYaxis().SetTitleOffset(0.8)
-  local_xSection_2g0p.GetYaxis().SetTitleSize(0.05)
-  local_xSection_2g0p.GetXaxis().SetLabelOffset(999)
-  local_xSection_2g0p.GetXaxis().SetLabelSize(0)
-  local_xSection_2g0p.GetXaxis().SetTickLength(0.)
-
-  local_xSection_mc_2g0p.Scale(10**38)
-
-
-#    int n = 4;
-#    double mcerr = E_genie_scale;
-#    double mcval = xs_genie_scale;
-#    std::vector<double> ymax(n,mcval+mcerr);
-#    std::vector<double> ymin(n,mcval-mcerr);
-#    std::vector<double> y(n,mcval);
-#    std::vector<double> x = {-1,0,1,2};
-#
-#    TGraph *gr    = new TGraph(n,&x[0],&y[0]);
-#    TGraph *grshade = new TGraph(2*n);
-#
-#    for (int i=0;i<n;i++) {
-#        grshade->SetPoint(i,x[i],ymax[i]);
-#        grshade->SetPoint(n+i,x[n-i-1],ymin[n-i-1]);
-#    }
-#
-#    grshade->SetFillStyle(3013);
-#    grshade->SetFillColor(kRed-7);
-#    grshade->SetLineColor(kRed-7);
-#    grshade->Draw("f");
-#    gr->SetLineWidth(3);
-#    gr->SetLineColor(kRed-7);
-#    gr->Draw("l");
-#
-#    Dat_2g0p->DrawCopy("E1p same");
-
-
-  local_xSection_mc_2g0p.SetFillStyle(3013)
-  local_xSection_mc_2g0p.SetFillColor(ROOT.kRed-7)
-  local_xSection_mc_2g0p.SetLineColor(ROOT.kRed-7)
-  #local_xSection_mc_2g0p.SetMarkerColor(ROOT.kRed)
-  #local_xSection_mc_2g0p.SetMarkerStyle(1)
-  local_xSection_mc_2g0p.Draw("f same")
-
-  #lineAtOne = ROOT.TLine(horizontalAxis_lowerBound,1,horizontalAxis_upperBound,1)
-  #lineAtOne.SetLineColor(ROOT.kRed)
-  #lineAtOne.Draw("same")
-
-  #local_xSection_2g1p.Scale(10**42)
-  #local_xSection_2g1p.SetMarkerColor(ROOT.kRed)
-  #local_xSection_2g1p.SetLineColor(ROOT.kRed)
-  #local_xSection_2g1p.Draw("same")
-
-  #local_xSection_2gnp.Scale(10**42)
-  #local_xSection_2gnp.SetMarkerColor(ROOT.kBlue)
-  #local_xSection_2gnp.SetLineColor(ROOT.kBlue)
-  #local_xSection_2gnp.Draw("same")
-
-  ############################################################### Legend
-  ######################################################################
-
-  #leg = ROOT.TLegend(0.11,0.7,0.89,0.89)
-  leg = ROOT.TLegend(0.15,0.7,0.95,0.9)
-  setPlotSpecs_legend(leg)
-  leg.AddEntry(local_xSection_2g0p,"Run 1-3 Data (2#gamma0p Extracted)","l")
-  leg.AddEntry(local_xSection_mc_2g0p,"Genie (G18_10a_02_11)","lf")
-  #leg.SetLineColor(ROOT.kWhite)
-  #leg.AddEntry(local_xSection_2g1p,"2g1p","lep")
-  #leg.AddEntry(local_xSection_2gnp,"2g(0+1)p","lep")
-  leg.Draw()
 
