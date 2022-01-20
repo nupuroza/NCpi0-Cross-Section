a013e64b96d0ec22eccd1c59c44615da762c5742
hyperfiner <rfine4@gmail.com>
2022-01-20 14:28:55 -0600

    Revert "Update file paths for multi-bin hists & skip over G4 systs"
    This reverts commit 0fc9d041016d9d4a2d241f0e3c152ef542572a67.

--- translateHists.py
+++ translateHists.py
@@ -30,10 +30,10 @@ cvFilePath_2g0p = "/uboone/app/users/markrl/SBNfit_uBooNE/July2020_SL7/whipping_
 cvFile_2g0p = ROOT.TFile(cvFilePath_2g0p)
 
 ## Efficiency Denominators
-effDenomFilePath_2g1p_inclusive = "/uboone/app/users/gge/singlephoton/whipping_star/working_directory/SinglePhoton_test/NCpi_cross_section/earlier_stage/Inclusive/MultipleBin/2g1p/variation_spectra/SBNfit_variation_spectra_Flux_XS.root"
+effDenomFilePath_2g1p_inclusive = "/uboone/app/users/gge/singlephoton/whipping_star/working_directory/SinglePhoton_test/NCpi_cross_section/earlier_stage/Inclusive/SingleBin/2g1p/variation_spectra/SBNfit_variation_spectra_Flux_XS.root"
 effDenomFile_2g1p_inclusive = ROOT.TFile(effDenomFilePath_2g1p_inclusive)
 
-effDenomFilePath_2g0p_inclusive = "/uboone/app/users/gge/singlephoton/whipping_star/working_directory/SinglePhoton_test/NCpi_cross_section/earlier_stage/Inclusive/MultipleBin/2g0p/variation_spectra/SBNfit_variation_spectra_Flux_XS.root"
+effDenomFilePath_2g0p_inclusive = "/uboone/app/users/gge/singlephoton/whipping_star/working_directory/SinglePhoton_test/NCpi_cross_section/earlier_stage/Inclusive/SingleBin/2g0p/variation_spectra/SBNfit_variation_spectra_Flux_XS.root"
 effDenomFile_2g0p_inclusive = ROOT.TFile(effDenomFilePath_2g0p_inclusive)
 
 effDenomFilePath_2g1p_exclusive = "/uboone/app/users/gge/singlephoton/whipping_star/working_directory/SinglePhoton_test/NCpi_cross_section/earlier_stage/Exclusive/2g1p/KE_50MeV/variation_spectra/SBNfit_variation_spectra_Flux_XS.root"
@@ -44,7 +44,7 @@ effDenomFile_2g0p_exclusive = ROOT.TFile(effDenomFilePath_2g0p_exclusive)
 
 ## Efficiency numerators; backgrounds
 ## Final stage; flux, XS, Det systematics included
-effNumFilePath_inclusive = "/uboone/app/users/gge/singlephoton/whipping_star/working_directory/SinglePhoton_test/NCpi_cross_section/final_stage/Inclusive/MultipleBin/variation_spectra/Merged_SBNfit_variation_spectra_FluxXSDet.root"
+effNumFilePath_inclusive = "/uboone/app/users/gge/singlephoton/whipping_star/working_directory/SinglePhoton_test/NCpi_cross_section/final_stage/Inclusive/SingleBin/variation_spectra/Merged_SBNfit_variation_spectra_FluxXSDet.root"
 effNumFile_inclusive = ROOT.TFile(effNumFilePath_inclusive)
 # 2g0p and 2g1p effNum hists come from the same file for the inclusive analysis
 effNumFile_2g0p_inclusive = effNumFile_inclusive 
@@ -69,12 +69,11 @@ g4File_2g1p_exclusive = ROOT.TFile(g4FilePath_2g1p_exclusive)
 
 ## Output file
 parser = argparse.ArgumentParser(description='Script to take TH1Ds evaluated in various systematic universes and package them into MnvH1Ds using the MINERvA Analysis Toolkit')
-parser.add_argument('output_dir', help='Path to ouput directory', type=str,nargs='?')
+parser.add_argument('output_dir', help='Path to ouput directory', type=str)
 p = parser.parse_args()
 
 ## If output_dir is not provided, exit
 if p.output_dir < 0:
-  print "ERROR: Output directory argument not provided"
   parser.print_help()
   exit(1)
 
@@ -141,15 +140,13 @@ DETECTOR_SYSTS = [
   ["WireX","minmax", nMinMaxUniverses],
   ["WireYZ","minmax", nMinMaxUniverses]
 ]
-'''
+
 G4_SYSTS = [
   ## [syst_name,universe_prefix,n_universes]
   ["piminus","universe",nMultiverses],
   ["piplus","universe",nMultiverses],
   ["proton","universe",nMultiverses]
 ]
-'''
-G4_SYSTS = []
  
 OTHER_SYSTS = [
   ## [syst_name,universe_prefix,n_universes]
