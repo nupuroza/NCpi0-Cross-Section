f55792ed84846812d187ab4a193e113c60a5939d
Nupur Oza <noza@uboonegpvm01.fnal.gov>
2022-04-07 12:46:10 -0500

    fix mHist file names

--- calculate_xsection.py
+++ calculate_xsection.py
@@ -26,16 +26,16 @@ histFile = ROOT.TFile(histFileLocation, "UPDATE")
 unfoldedFileLocation = p.inout_dir+"/"+p.in_date+"_unfolded.root"
 unfoldedFile = ROOT.TFile(unfoldedFileLocation, "READ") 
 
-mHist_eff_2gnp_inclusive = histFile.Get("mHist_eff_2gnp_inclusive")
-mHist_flux_integral = histFile.Get("mHist_flux_integral")
-mHist_POT_2gnp = histFile.Get("mHist_POT_2gnp")
-mHist_nTargets = histFile.Get("mHist_nTargets")
+mHist_eff_2gnp_inclusive = histFile.Get("eff_2gnp_inclusive")
+mHist_flux_integral = histFile.Get("flux_integral")
+mHist_POT_2gnp = histFile.Get("POT_2gnp")
+mHist_nTargets = histFile.Get("nTargets")
 
 ref_mHist = mHist_eff_2gnp_inclusive.Clone("ref_mHist")
 tHist_evtRate_2gnp_inclusive_unfolded = unfoldedFile.Get("unf_signal")
 mHist_evtRate_2gnp_inclusive_unfolded = ROOT.PlotUtils.MnvH1D(tHist_evtRate_2gnp_inclusive_unfolded)
 mHist_evtRate_2gnp_inclusive_unfolded.AddMissingErrorBandsAndFillWithCV(ref_mHist)
-mHist_evtRate_2gnp_inclusive_unfolded.SetName("mHist_evtRate_2gnp_inclusive_unfolded")
+mHist_evtRate_2gnp_inclusive_unfolded.SetName("evtRate_2gnp_inclusive_unfolded")
 writeHist(mHist_evtRate_2gnp_inclusive_unfolded,histFile)
 
 ## Cross section calculation
