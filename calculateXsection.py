7dcbea603714a34f259ec1e69fa6626307dcac4c
Nupur Oza <noza@uboonegpvm01.fnal.gov>
2022-07-15 16:02:40 -0500

    remove mc cross section calculation

--- calculateXsection.py
+++ calculateXsection.py
@@ -11,7 +11,7 @@ ROOT.TH1.AddDirectory(False)
 
 def writeHist(hist,outFile):
   outFile.cd()
-  print 'Writing 2g1p to output file'.format(hist.GetName())
+  print 'Writing {0} to output file'.format(hist.GetName())
   hist.Write()
 
 ## Set arguments
@@ -23,11 +23,11 @@ p = parser.parse_args()
 histFileLocation = p.inout_dir+"/"+p.in_date+"_out_unfolded.root"
 histFile = ROOT.TFile(histFileLocation, "UPDATE")
 
+mHist_evtRate_2g1p_inclusive_unfolded = histFile.Get("unfolded_evtRate_2g1p_inclusive")
 mHist_eff_2g1p_inclusive = histFile.Get("eff_2g1p_inclusive")
 mHist_flux_integral = histFile.Get("integratedFlux")
 mHist_POT_2g1p = histFile.Get("POT_2g1p")
 mHist_nTargets = histFile.Get("nTargets")
-mHist_evtRate_2g1p_inclusive_unfolded = histFile.Get("unfolded_evtRate_2g1p_inclusive")
 
 ## Cross section calculation
 mHist_xSection_2g1p_inclusive_unfolded = mHist_evtRate_2g1p_inclusive_unfolded.Clone("unfolded_xSection_2g1p_inclusive")
@@ -37,18 +37,5 @@ mHist_xSection_2g1p_inclusive_unfolded.Divide(mHist_xSection_2g1p_inclusive_unfo
 mHist_xSection_2g1p_inclusive_unfolded.Divide(mHist_xSection_2g1p_inclusive_unfolded,mHist_nTargets)
 
 writeHist(mHist_xSection_2g1p_inclusive_unfolded,histFile)
-
-## MC signal prediction
-mHist_xSection_mc_2g1p_inclusive = mHist_effNum_2g1p_inclusive.Clone("xSection_mc_2g1p_inclusive")
-## We don't want to subtract off the background prediction because the MC signal doesn't include backgrounds
-mHist_xSection_mc_2g1p_inclusive.Divide(mHist_xSection_mc_2g1p_inclusive,mHist_eff_2g1p_inclusive)
-mHist_xSection_mc_2g1p_inclusive.Divide(mHist_xSection_mc_2g1p_inclusive,mHist_flux_integral)
-mHist_xSection_mc_2g1p_inclusive.Divide(mHist_xSection_mc_2g1p_inclusive,mHist_POT_2g1p) # Remove units of per POT
-mHist_xSection_mc_2g1p_inclusive.Divide(mHist_xSection_mc_2g1p_inclusive,mHist_nTargets)
-
-## Pop out all error bands except GENIE from the MC xsection
-for systName,universePrefix,nUniverses in FLUX_SYSTS + DETECTOR_SYSTS + G4_SYSTS + OTHER_SYSTS:
-  exec("mHist_xSection_mc_2g1p_inclusive.PopVertErrorBand(\"{2}\")".format(systName))
-  writeHist(mHist_xSection_mc_2g1p_inclusive,outFile)
 
 histFile.Close()
