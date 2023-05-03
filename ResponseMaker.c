07ee10391376b54be8d7904eeaffe66323955d5d
hyperfiner <rfine4@gmail.com>
2023-05-03 15:50:56 -0500

    Modifications to ResponseMaker.c to simultaneously produce 2g0p and 2g1p response matrices

--- ResponseMaker.c
+++ ResponseMaker.c
@@ -49,21 +49,34 @@ void ResponseMaker(std::string outDir){
     TTreeFormula * true_form = new TTreeFormula("truef",true_var.c_str(), v);
 
     // Prescription for calculating event weight
-    std::string additional_weight=
+    // -----------------------------------------
+
+    // Bit that's common to 2g1p and 2g0p
+    std::string additional_weight_common =
     "(\
       (simple_pot_weight*6.7873e20/4.9669582e+21)\
      *(m_flash_optfltr_pe_beam >20 && m_flash_optfltr_pe_veto < 20)\
      *(MCFlux_NuPosX > (0.0-1.55) && MCFlux_NuPosX < (256.35-1.55) && MCFlux_NuPosY > (-116.5+0.97) && MCFlux_NuPosY < (116.5+0.97) && MCFlux_NuPosZ > 0.0+0.1 && MCFlux_NuPosZ < 1036.8+0.1)\
      *( (run_number >= 4952 && run_number <= 7770)*0.943100 + ( (run_number >= 8317 && run_number <=  13696) || (run_number >= 13697 && run_number <= 14116) || (run_number >= 14117 && run_number <= 18960) )*1.020139 )\
-     *(Sum$(mctruth_exiting_proton_energy-0.93827 > 0.05)==1)\
     )";
 
-    // Prescription for determining which events pass selection cuts
-    // This would change for 2g0p
-    TTreeFormula * pass_form = new TTreeFormula("pass","(simple_2g1p_NextGen_v4COSMIC_mva>0.894 && simple_2g1p_NextGen_v4BNB_mva >0.737)", v);
+    // Bit that's unique to 2g1p
+    std::string additional_weight_2g1p = additional_weight_common + "*(Sum$(mctruth_exiting_proton_energy-0.93827 > 0.05)==1)";
 
-    // Prescription for normalizing selection and implementing some cuts
-    TTreeFormula * norm_form = new TTreeFormula("norm",(additional_weight).c_str(), v);
+    // Bit that's unique to 2g0p
+    std::string additional_weight_2g0p = additional_weight_common + "*(Sum$(mctruth_exiting_proton_energy-0.93827 > 0.05)==0)";
+
+    // Prescription for determining which events 
+    // pass selection cuts
+    // -----------------------------------------
+    TTreeFormula * pass_form_2g1p = new TTreeFormula("pass","(simple_2g1p_NextGen_v4COSMIC_mva>0.894 && simple_2g1p_NextGen_v4BNB_mva >0.737)", v);
+    TTreeFormula * pass_form_2g0p = new TTreeFormula("pass","(simple_2g0p_NextGen_v4COSMIC_mva>0.944 && simple_2g0p_NextGen_v4BNB_mva >0.731)", v);
+
+    // Prescription for normalizing selection 
+    // and implementing some cuts
+    // -----------------------------------------
+    TTreeFormula * norm_form_2g1p = new TTreeFormula("norm",(additional_weight_2g1p).c_str(), v);
+    TTreeFormula * norm_form_2g0p = new TTreeFormula("norm",(additional_weight_2g0p).c_str(), v);
 
     // -----------------------------------------------------
     // Construct response matrix 
@@ -74,13 +87,18 @@ void ResponseMaker(std::string outDir){
     std::vector<double> true_bins = {0, 0.075, 0.15, 0.225, 0.3, 0.375, 0.45, 0.525, 0.6, 0.675,  0.9};
 
     // Create htrue, hreco, and response hists
-    TH1D * htrue = new TH1D("htrue","htrue",true_bins.size()-1, &true_bins[0]);
-    TH1D * hreco = new TH1D("hreco","hreco",reco_bins.size()-1, &reco_bins[0]);
-    TH2D * resp = new TH2D("Response","Response",reco_bins.size()-1, &reco_bins[0], true_bins.size()-1, &true_bins[0]);
+    TH1D * htrue_2g1p = new TH1D("htrue_2g1p","htrue_2g1p",true_bins.size()-1, &true_bins[0]);
+    TH1D * htrue_2g0p = new TH1D("htrue_2g0p","htrue_2g0p",true_bins.size()-1, &true_bins[0]);
+    TH1D * hreco_2g1p = new TH1D("hreco_2g1p","hreco_2g1p",reco_bins.size()-1, &reco_bins[0]);
+    TH1D * hreco_2g0p = new TH1D("hreco_2g0p","hreco_2g0p",reco_bins.size()-1, &reco_bins[0]);
+    TH2D * resp_2g1p = new TH2D("Response_2g1p","Response_2g1p",reco_bins.size()-1, &reco_bins[0], true_bins.size()-1, &true_bins[0]);
+    TH2D * resp_2g0p = new TH2D("Response_2g0p","Response_2g0p",reco_bins.size()-1, &reco_bins[0], true_bins.size()-1, &true_bins[0]);
 
     // Create response TMatrix (large enough to include underflow/underflow from corresponding TH2D)
-    TMatrixD mat(reco_bins.size()+1,true_bins.size()+1);
-    mat.Zero();
+    TMatrixD mat_2g1p(reco_bins.size()+1,true_bins.size()+1);
+    mat_2g1p.Zero();
+    TMatrixD mat_2g0p(reco_bins.size()+1,true_bins.size()+1);
+    mat_2g0p.Zero();
 
     // Loop through event tree; fill htrue, hreco, and response hists
     for(int i=0; i<v->GetEntries();i++){
@@ -90,65 +108,89 @@ void ResponseMaker(std::string outDir){
         // Technically not necessary, but Mark says that this avoids some inconsistent obscure ROOT bug
         reco_form->GetNdata();
         true_form->GetNdata();
-        pass_form->GetNdata();
-        norm_form->GetNdata();
+        pass_form_2g1p->GetNdata();
+        pass_form_2g0p->GetNdata();
+        norm_form_2g1p->GetNdata();
+        norm_form_2g0p->GetNdata();
 
         double r = reco_form->EvalInstance();
         double t = true_form->EvalInstance();
-        double p = pass_form->EvalInstance();
-        double w = norm_form->EvalInstance();
+        double p_2g1p = pass_form_2g1p->EvalInstance();
+        double p_2g0p = pass_form_2g0p->EvalInstance();
+        double w_2g1p = norm_form_2g1p->EvalInstance();
+        double w_2g0p = norm_form_2g0p->EvalInstance();
 
         if(i%20000==0)std::cout<<i<<" "<<v->GetEntries()<<std::endl;
     //    std::cout<<r<<"\t\t"<<t<<"\t\t"<<p<<std::endl;
 
-        if(p){
-            resp->Fill(r,t,w);
-            htrue->Fill(t,w);
-            hreco->Fill(r,w);
+        if(p_2g1p){
+            resp_2g1p->Fill(r,t,w_2g1p);
+            htrue_2g1p->Fill(t,w_2g1p);
+            hreco_2g1p->Fill(r,w_2g1p);
+        }
+        else if(p_2g0p){
+            resp_2g0p->Fill(r,t,w_2g0p);
+            htrue_2g0p->Fill(t,w_2g0p);
+            hreco_2g0p->Fill(r,w_2g0p);
         }
         else{
             //resp->Fill(-999,t,w);
-            htrue->Fill(t,w);
+            htrue_2g1p->Fill(t,w_2g1p);
+            htrue_2g0p->Fill(t,w_2g0p);
         }
 
     }
-    std::cout << "Finished main loop; constructed htrue, hreco, and response hists." << std::endl;
+    std::cout << "Finished main loop; constructed htrue, hreco, and response hists for both 2g1p and 2g0p signal definitions." << std::endl;
 
-    for(int i=0;i< hreco->GetNbinsX()+2; i++){ // Both loops need to go to nbins+1 to capture the overflow
-        for(int a=0; a<htrue->GetNbinsX()+2; a++){
-         
-            double eff = resp->GetBinContent(i,a)/htrue->GetBinContent(a);
-            resp->SetBinContent(i,a, eff); // Resets content of Response TH2D so there is consistency
-            mat(i,a) = resp->GetBinContent(i,a); 
+    for(int i=0;i< hreco_2g1p->GetNbinsX()+2; i++){ // Both loops need to go to nbins+1 to capture the overflow
+        for(int a=0; a<htrue_2g1p->GetNbinsX()+2; a++){ // It doesn't matter whether we loop over 2g1p or 2g0p to get the bin counts
+            
+            //2g1p  
+            double eff_2g1p = resp_2g1p->GetBinContent(i,a)/htrue_2g1p->GetBinContent(a);
+            resp_2g1p->SetBinContent(i,a, eff_2g1p); // Resets content of Response TH2D so there is consistency
+            mat_2g1p(i,a) = resp_2g1p->GetBinContent(i,a); 
+
+            //2g0p  
+            double eff_2g0p = resp_2g0p->GetBinContent(i,a)/htrue_2g0p->GetBinContent(a);
+            resp_2g0p->SetBinContent(i,a, eff_2g0p); // Resets content of Response TH2D so there is consistency
+            mat_2g0p(i,a) = resp_2g0p->GetBinContent(i,a); 
 
         }
     }
-    std::cout << "Constructed response TMatrix." << std::endl;
+    std::cout << "Constructed response TMatrices." << std::endl;
     
     // -----------------------------------------------------
     // Construct output file 
     // -----------------------------------------------------
 
     // Create output file
-    TFile *fout = new TFile((outDir+"/response_2g1p_exclusive_v3_d22_23.root").c_str(),"RECREATE");
+    TFile *fout = new TFile((outDir+"/response_matrices_exclusive.root").c_str(),"RECREATE");
     fout->cd();
 
-    // Write out response matrix TH2D
-    resp->GetXaxis()->SetTitle("Reco Var");
-    resp->GetYaxis()->SetTitle("True Var");
-    std::cout << "Writing response matrix TH2D to output file" << std::endl;
-    resp->Write();
+    // Write out response matrix TH2Ds
+    std::cout << "Writing response matrix TH2Ds to output file" << std::endl;
+    resp_2g1p->GetXaxis()->SetTitle("Reco Var");
+    resp_2g1p->GetYaxis()->SetTitle("True Var");
+    resp_2g1p->Write();
+    resp_2g0p->GetXaxis()->SetTitle("Reco Var");
+    resp_2g0p->GetYaxis()->SetTitle("True Var");
+    resp_2g0p->Write();
 
-    // Write out response matrix TMatrix
-    std::cout << "Writing response matrix TMatrix to output file" << std::endl;
-    mat.Write("response_matrix");
+    // Write out response matrix TMatrices
+    std::cout << "Writing response matrix TMatrices to output file" << std::endl;
+    mat_2g1p.Write("response_matrix");
+    mat_2g0p.Write("response_matrix");
     
     // Write out htrue and hreco
     std::cout << "Writing htrue and hreco TH1Ds to output file" << std::endl;
-    htrue->GetXaxis()->SetTitle("True Var");
-    hreco->GetXaxis()->SetTitle("Reco Var");
-    htrue->Write();
-    hreco->Write();
+    htrue_2g1p->GetXaxis()->SetTitle("True Var");
+    hreco_2g1p->GetXaxis()->SetTitle("Reco Var");
+    htrue_2g1p->Write();
+    hreco_2g1p->Write();
+    htrue_2g0p->GetXaxis()->SetTitle("True Var");
+    hreco_2g0p->GetXaxis()->SetTitle("Reco Var");
+    htrue_2g0p->Write();
+    hreco_2g0p->Write();
 
     // Close output file
     fout->Close();
