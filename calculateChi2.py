245d9c2c28637d4921eb08434032150ceeefb0a2
Leon Tong <leontong0909@yahoo.com>
2024-06-17 00:15:53 -0500

    Made adjustments to DrawWithOverflow() function in customHistAndPlotMethods.py and fixed title positioning. Modified calculateChi2() to include overflow bin in Chi2 calculation. Finished implementing DrawWithOverflow() for all 1D histograms in makeFakeDataStudyPlots.py and adjusted covariance matrix inputs to calculateChi2 to include overflow bins.

--- calculateChi2.py
+++ calculateChi2.py
@@ -13,16 +13,19 @@ def calculateChi2(mc_hist, data_hist, cov_matrix, cov_matrix_includes_stats):
     diff_hist.Add(mc_hist, -1)
     # Overflow bins currently excluded from analysis. Will likely be changed at a later date.
     nbins = diff_hist.GetNbinsX()
-    if nbins != cov_matrix.GetNrows() or nbins != cov_matrix.GetNcols():
+    if nbins + 1 != cov_matrix.GetNrows() or nbins + 1 != cov_matrix.GetNcols():
+        print("nbins + 1: " + str(nbins + 1))
+        print("cov_matrix_rows: " + str(cov_matrix.GetNrows()))
+        print("cov_matrix_cols: " + str(cov_matrix.GetNcols()))
         print("ERROR: Histogram binning does not match covariance matrix in calculateChi2(). Exiting!")
         sys.exit(1)
     # If statistical uncertainties aren't already included, calculate them using Eq. 19 of paper.
     if not cov_matrix_includes_stats:
-        for i in range(nbins):
+        for i in range(nbins + 1):
             cov_matrix[i][i] += 3/(1/data_hist.GetBinContent(i+1) + 2/mc_hist.GetBinContent(i+1))
     inv_cov_matrix = ROOT.TMatrixD(ROOT.TMatrixD.kInverted, cov_matrix)
-    diff_mat = ROOT.TMatrixD(nbins, 1)
-    for i in range(nbins):
+    diff_mat = ROOT.TMatrixD(nbins + 1, 1)
+    for i in range(nbins + 1):
         diff_mat[i][0] = diff_hist.GetBinContent(i + 1)
     # Calculate chi2 using Eq. 18 of paper.
     chi2_temp = ROOT.TMatrixD(diff_mat, ROOT.TMatrixD.kTransposeMult, inv_cov_matrix)
